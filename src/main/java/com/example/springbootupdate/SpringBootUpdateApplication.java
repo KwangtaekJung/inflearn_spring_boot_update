edd106b4c023750cb50bc6e57a2873ba8b79b637
KwangtaekJung <kt79.jung@gmail.com>
2023-10-20 00:12:36 +0900

    remove warning

--- src/main/java/com/example/springbootupdate/SpringBootUpdateApplication.java
+++ src/main/java/com/example/springbootupdate/SpringBootUpdateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootupdateApplication {
+public class SpringBootUpdateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootupdateApplication.class, args);
+		SpringApplication.run(SpringBootUpdateApplication.class, args);
 	}
 
 }
